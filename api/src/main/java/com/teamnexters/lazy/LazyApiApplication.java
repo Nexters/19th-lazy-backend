9e59fd5071f1cdb08f5df91906cbda8cd15ea05b
YoungJun Park <youngjun108059@gmail.com>
2021-07-22 07:10:49 +0900

    [Init] Spring Boot 멀티모듈 Project [#1]

---
+++ api/src/main/java/com/teamnexters/lazy/LazyApiApplication.java
@@ -0,0 +1,13 @@
+package com.teamnexters.lazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazyApiApplication.class, args);
+    }
+
+}
---
+++ batch/src/main/java/com/teamnexters/lazy/LazyBatchApplication.java
@@ -0,0 +1,13 @@
+package com.teamnexters.lazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazyBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LazyBatchApplication.class, args);
+    }
+
+}
