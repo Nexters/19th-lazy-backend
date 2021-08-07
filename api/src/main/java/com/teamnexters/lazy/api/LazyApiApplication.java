9c5b48f42eb8b698241357dcb730a8f56b5ae36d
YoungJun Park <youngjun108059@gmail.com>
2021-08-07 22:03:33 +0900

    [Fix] 멀티모듈 실행 에러 수정

--- api/src/main/java/com/teamnexters/lazy/api/LazyApiApplication.java
+++ api/src/main/java/com/teamnexters/lazy/api/LazyApiApplication.java
@@ -2,8 +2,14 @@ package com.teamnexters.lazy.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.teamnexters.lazy")
+@EnableJpaRepositories(basePackages = "com.teamnexters.lazy")
+@EntityScan(basePackages = "com.teamnexters.lazy")
 public class LazyApiApplication {
 
     public static void main(String[] args) {
