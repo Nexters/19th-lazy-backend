c98ef492c4128b5e5e30e85eb1818c76736f7b7f
YoungJun Park <youngjun108059@gmail.com>
2021-08-09 01:13:32 +0900

    [#2] Member OAuth 기반 도메인 변경

--- common/src/main/java/com/teamnexters/lazy/common/domain/member/Member.java
+++ common/src/main/java/com/teamnexters/lazy/common/domain/member/Member.java
@@ -1,6 +1,9 @@
 package com.teamnexters.lazy.common.domain.member;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.AccessLevel;
@@ -15,15 +18,42 @@ public class Member {
 
     @Id
     @GeneratedValue
-    private Long memberId;
+    @Column(name = "mem_idx")
+    private Long memberIdx;
 
+    @Column(name = "mem_name", nullable = false)
     private String name;
+
+    @Column(name = "mem_nickname")
+    private String nickName;
+
+    @Column(name = "mem_email", nullable = false)
     private String email;
 
+    @Column(name = "mem_image_url", nullable = false)
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
     @Builder
-    public Member(String name, String email) {
+    public Member(String name, String nickName, String email, String picture, Role role) {
         this.name = name;
+        this.nickName = nickName;
         this.email = email;
+        this.picture = picture;
+        this.role = role;
     }
 
+    public Member update(String nickName, String picture) {
+        this.nickName = nickName;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
 }
