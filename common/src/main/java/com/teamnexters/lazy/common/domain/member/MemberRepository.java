f286944f103bc8d3d93ce282b666c59f588c80ce
YoungJun Park <youngjun108059@gmail.com>
2021-08-10 22:41:15 +0900

    [#2] Member JPA Optional 적용

--- common/src/main/java/com/teamnexters/lazy/common/domain/member/MemberRepository.java
+++ common/src/main/java/com/teamnexters/lazy/common/domain/member/MemberRepository.java
@@ -1,7 +1,8 @@
 package com.teamnexters.lazy.common.domain.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
