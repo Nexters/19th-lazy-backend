1d482f0ba1d15ef7cb7b1d7b7ccac6e54f2884ff
YoungJun Park <youngjun108059@gmail.com>
2021-08-11 00:03:57 +0900

    [#2] Member 전체 조회 API

--- api/src/main/java/com/teamnexters/lazy/api/controller/MemberController.java
+++ api/src/main/java/com/teamnexters/lazy/api/controller/MemberController.java
@@ -3,29 +3,32 @@ package com.teamnexters.lazy.api.controller;
 import com.teamnexters.lazy.api.service.MemberService;
 import com.teamnexters.lazy.common.domain.member.Member;
 import com.teamnexters.lazy.common.domain.member.MemberDto;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api")
 public class MemberController {
 
     private MemberService memberService;
 
     // POST http://localhost:9093/v1/member
     @PostMapping("/v1/member")
-    public ResponseEntity<MemberDto.Res> getMemberList(@RequestBody @Valid MemberDto.SignUpReq dto) {
+    public ResponseEntity<MemberDto.Res> saveMemberList(@RequestBody @Valid MemberDto.SignUpReq dto) {
         return ResponseEntity.ok().body(memberService.create(dto));
     }
 
     // GET http://localhost:9093/v1/member
-//    @GetMapping("/v1/member")
-//    public ResponseEntity<MemberDto.Res> getMemberList() {
-//        return name;
-//    }
+    @GetMapping("/v1/member")
+    public ResponseEntity<List<Member>> getMemberList() {
+        return ResponseEntity.ok().body(memberService.getMemberList());
+    }
 }
