9993bc051e2b405b5d210ab03c89ed9b727d0796
YoungJun Park <youngjun108059@gmail.com>
2021-08-19 00:09:36 +0900

    [#4] Member Provider 연동

---
+++ common/src/main/java/com/teamnexters/lazy/common/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.teamnexters.lazy.common.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @CreatedDate
+    private LocalDateTime createdDt;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @LastModifiedDate
+    private LocalDateTime modifiedDt;
+}
---
+++ common/src/main/java/com/teamnexters/lazy/common/domain/member/Provider.java
@@ -0,0 +1,10 @@
+package com.teamnexters.lazy.common.domain.member;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Provider {
+    GOOGLE, KAKAO, NAVER, FACEBOOK
+}
