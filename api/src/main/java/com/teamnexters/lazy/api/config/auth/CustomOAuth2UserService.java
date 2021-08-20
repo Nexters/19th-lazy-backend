1b16396b8a155c7688935036b12eae5e321811f3
YoungJun Park <youngjun108059@gmail.com>
2021-08-21 02:10:38 +0900

    [Refactor] JavaDoc 작성

--- api/src/main/java/com/teamnexters/lazy/api/config/auth/CustomOAuth2UserService.java
+++ api/src/main/java/com/teamnexters/lazy/api/config/auth/CustomOAuth2UserService.java
@@ -23,31 +23,52 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
     private final MemberRepository memberRepository;
     private final HttpSession httpSession;
 
+    /**
+     * 1. 구글 로그인 버튼 클릭
+     * 2. 구글 로그인 화면 -> 로그인 완료
+     * 3. code 리턴 (OAuth-Client 라이브러리)
+     * 4. AccessToken 요청
+     * 5. UserRequest 정보 리턴
+     * 6. loadUser 함수 호출
+     * 7. {OAuth 프로바이더} 회원 프로필 리턴
+     * 8. @AuthenticationPrincipal 어노테이션 생성
+     *
+     * @param userRequest OAuth 프로바이더에서 내려준 정보
+     * @return OAuth User
+     * @throws OAuth2AuthenticationException
+     */
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // OAuth 에서 처리한 데이터를 후처리하는 함수
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
         // OAuth2 서비스 id (구글, 카카오, 네이버)
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        // OAuth2 로그인 진행 시 키가 되는 필드 값(PK)
-        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+        // 로그인 시, PK가 되는 필드 값 받아오기 (구글:"sub", 네이버 카카오 지원x)
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
-        // OAuth2UserService
-        OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
-        Member user = saveOrUpdate(attributes);
-        httpSession.setAttribute("user", new SessionUser(user)); // SessionUser (직렬화된 dto 클래스 사용)
+        // 소셜 로그인 된 유저 정보 - 객체화
+        OAuthAttributes attributes = OAuthAttributes.
+                of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
+        // 사용자 정보 등록 or 업데이트
+        Member member = saveOrUpdate(attributes);
+        // 세션에 사용자 정보 등록
+        httpSession.setAttribute("user", new SessionUser(member));
 
-        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey())),
-            attributes.getAttributes(),
-            attributes.getNameAttributeKey());
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(member.getRoleKey())),
+                attributes.getAttributes(),
+                attributes.getNameAttributeKey());
     }
 
-    // 유저 생성 및 수정 서비스 로직
-    private Member saveOrUpdate(OAuthAttributes attributes){
+    private Member saveOrUpdate(OAuthAttributes attributes) {
+        // 중복 체크하여 저장 혹은 업데이트
         Member user = memberRepository.findByEmail(attributes.getEmail())
-            .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
-            .orElse(attributes.toEntity());
+                .map(entity -> entity.update(attributes.getName(),attributes.getPicture()))
+                .orElse(attributes.toEntity());
+        // DB에 저장 후 사용자 객체 반환
         return memberRepository.save(user);
     }
 }
--- common/src/main/java/com/teamnexters/lazy/common/domain/member/MemberRepository.java
+++ common/src/main/java/com/teamnexters/lazy/common/domain/member/MemberRepository.java
@@ -4,5 +4,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    /**
+     * 이미 등록된 email 인지 검사
+     *
+     * @param email 이메일
+     * @return 일치하는 회원 정보
+     */
     Optional<Member> findByEmail(String email);
 }
