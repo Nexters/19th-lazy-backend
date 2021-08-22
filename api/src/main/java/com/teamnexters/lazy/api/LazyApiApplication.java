18aa5b8fb5d2da3adcb1e859c9584ec9e1881ce8
YoungJun Park <youngjun108059@gmail.com>
2021-08-22 23:49:28 +0900

    [Fix] EnableJpaAuditing 추가

--- api/src/main/java/com/teamnexters/lazy/api/LazyApiApplication.java
+++ api/src/main/java/com/teamnexters/lazy/api/LazyApiApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaAuditing // JPA Auditing 활성화
 @ComponentScan(basePackages = "com.teamnexters.lazy")
 @EnableJpaRepositories(basePackages = "com.teamnexters.lazy")
 @EntityScan(basePackages = "com.teamnexters.lazy")
