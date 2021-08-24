9a7dbce772f36b8520597931d567465268adab6f
YoungJun Park <youngjun108059@gmail.com>
2021-08-24 22:13:31 +0900

    [Fix] Swagger + OAuth 충돌 해결

--- api/src/main/java/com/teamnexters/lazy/api/config/auth/SecurityConfig.java
+++ api/src/main/java/com/teamnexters/lazy/api/config/auth/SecurityConfig.java
@@ -15,32 +15,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers(
-                        // 누구나 접근 가능한 설정
-                        "/",
-                        "/css/**",
-                        "/images/**",
-                        "/js/**",
-                        "/h2-console/**",
-                        "/login/**",
-                        "/swagger-ui/**",
-                        "/swagger-ui.html"
-                ).permitAll()
-                .antMatchers(
-                        // "USER"인 경우만 접근 가능
-                        "/api/v1/**",
-                        "/admin/**"
-                ).hasRole(Role.USER.name())
-                // 나머지 URL 요청은 무조건 인증
-                .anyRequest().authenticated()
+                    .authorizeRequests()
+                    .antMatchers(
+                            // 누구나 접근 가능한 설정
+                            "/",
+                            "/css/**",
+                            "/images/**",
+                            "/js/**",
+                            "/h2-console/**",
+                            "/login/**",
+                            "/swagger-ui/**",
+                            "/swagger-ui.html",
+                            "/api-docs/**"
+                    ).permitAll()
+                    .antMatchers(
+                            // "USER"인 경우만 접근 가능
+                            "/api/v1/**",
+                            "/admin/**"
+                    ).hasRole(Role.USER.name())
+                    // 나머지 URL 요청은 무조건 인증
+                    .anyRequest().authenticated()
                 .and()
-                .logout()
-                .logoutSuccessUrl("/") // 로그아웃 후 리다이렉팅 될 URL
+                    .logout()
+                    .logoutSuccessUrl("/") // 로그아웃 후 리다이렉팅 될 URL
                 .and()
-                // OAuth2 로그인 관련 설정
-                .oauth2Login()
-                .userInfoEndpoint() // 로그인 성공 시, 사용자 정보 설정
-                .userService(customOAuth2UserService); // 로그인 성공 후, 사용자를 다룰 서비스 등록
+                    // OAuth2 로그인 관련 설정
+                    .oauth2Login()
+                    .userInfoEndpoint() // 로그인 성공 시, 사용자 정보 설정
+                    .userService(customOAuth2UserService); // 로그인 성공 후, 사용자를 다룰 서비스 등록
     }
 }
