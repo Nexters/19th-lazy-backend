0937403da4f4051e9e6084b4a8590e96440b919b
YoungJun Park <youngjun108059@gmail.com>
2021-09-08 01:10:36 +0900

    [#5] 습관 중복 검사, 조회 JPA 쿼리 메소드

--- common/src/main/java/com/teamnexters/lazy/common/domain/habit/HabitRepository.java
+++ common/src/main/java/com/teamnexters/lazy/common/domain/habit/HabitRepository.java
@@ -1,5 +1,7 @@
 package com.teamnexters.lazy.common.domain.habit;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -15,4 +17,21 @@ public interface HabitRepository extends JpaRepository<Habit, Long> {
      */
     Optional<Habit> findByMemIdxAndHabitName(Long memIdx, String habitName);
 
+    /**
+     * 이미 등록된 습관인지 (idx 기준)
+     *
+     * @param memIdx    회원 번호
+     * @param habitIdx  습관 번호
+     * @return 존재하는 습관 반환
+     */
+    Optional<Habit> findByMemIdxAndHabitIdx(Long memIdx, Long habitIdx);
+
+    /**
+     * 회원의 습관 조회 (페이징)
+     *
+     * @param memIdx    회원 번호
+     * @param pageable  페이징
+     * @return 페이징된 습관
+     */
+    Page<Habit> findByMemIdx(Long memIdx, Pageable pageable);
 }
