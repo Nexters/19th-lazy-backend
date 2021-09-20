7ca27aaa4ef4fdf409f902abdf125eee56418a52
YoungJun Park <youngjun108059@gmail.com>
2021-09-21 02:32:08 +0900

    [#4] OAuth Entity save 에러 해결

--- api/src/main/java/com/teamnexters/lazy/api/config/auth/dto/OAuthAttributes.java
+++ api/src/main/java/com/teamnexters/lazy/api/config/auth/dto/OAuthAttributes.java
@@ -1,12 +1,19 @@
 package com.teamnexters.lazy.api.config.auth.dto;
+import com.teamnexters.lazy.api.exception.OAuth2ProviderNotMatchException;
 import com.teamnexters.lazy.common.domain.member.Member;
 import com.teamnexters.lazy.common.domain.member.Provider;
 import com.teamnexters.lazy.common.domain.member.Role;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
+@Slf4j
+@Builder(access = AccessLevel.PRIVATE)
+@ToString
 @Getter
 public class OAuthAttributes {
     private final Map<String, Object> attributes; // OAuth2 반환하는 유저 정보 Map
@@ -16,29 +23,45 @@ public class OAuthAttributes {
     private final String picture;
     private final Provider provider;
 
-    @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture, Provider provider) {
-        this.attributes = attributes;
-        this.nameAttributeKey = nameAttributeKey;
-        this.name = name;
-        this.email = email;
-        this.picture = picture;
-        this.provider = provider;
+    public static OAuthAttributes of(String provider, String userNameAttributeName, Map<String, Object> attributes){
+
+        switch (provider) {
+            case "google":
+                return ofGoogle(userNameAttributeName, attributes);
+            case "kakao":
+                return ofKakao(attributes);
+            case "naver":
+                return ofNaver(attributes);
+            default:
+                throw new OAuth2ProviderNotMatchException(provider);
+        }
     }
 
-    public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
-        // Naver 로그인
-        if (registrationId.equals("naver")) {
-            return ofNaver(attributes);
-        }
+    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .picture((String) attributes.get("picture"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .provider(Provider.GOOGLE)
+                .build();
+    }
 
-        // Kakao 로그인
-        if (registrationId.equals("kakao")) {
-            return ofKakao(attributes);
-        }
+    private static OAuthAttributes ofKakao(Map<String, Object> attributes) {
+        // JSON 형태를 Map 으로 가져오기 (kakao_account)
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        // Profile 조회
+        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
 
-        // Google 로그인
-        return ofGoogle(userNameAttributeName, attributes);
+        return OAuthAttributes.builder()
+                .name((String) kakaoProfile.get("nickname"))
+                .email((String) kakaoAccount.get("email"))
+                .picture((String) kakaoProfile.get("profile_image_url"))
+                .attributes(kakaoAccount)
+                .nameAttributeKey("email")
+                .provider(Provider.KAKAO)
+                .build();
     }
 
     private static OAuthAttributes ofNaver(Map<String, Object> attributes) {
@@ -54,39 +77,12 @@ public class OAuthAttributes {
             .build();
     }
 
-    private static OAuthAttributes ofKakao(Map<String, Object> attributes) {
-        // JSON 형태를 Map 으로 가져오기 (kakao_account)
-        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
-        // Profile 조회
-        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
-
-        return OAuthAttributes.builder()
-            .name((String) kakaoProfile.get("nickname"))
-            .email((String) kakaoProfile.get("email"))
-            .picture((String) kakaoProfile.get("profile_image_url"))
-            .attributes(attributes)
-            .nameAttributeKey("id")
-            .provider(Provider.KAKAO)
-            .build();
-    }
-
-    private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
-        return OAuthAttributes.builder()
-                .name((String) attributes.get("name"))
-                .email((String) attributes.get("email"))
-                .picture((String) attributes.get("picture"))
-                .attributes(attributes)
-                .nameAttributeKey(userNameAttributeName)
-                .provider(Provider.GOOGLE)
-                .build();
-    }
-
     public Member toEntity(){
         return Member.builder()
             .name(name)
             .email(email)
             .picture(picture)
-            .role(Role.GUEST) // 기본 권한 GUEST
+            .role(Role.USER) // 기본 권한 GUEST, 일반 : USER
             .provider(provider)
             .build();
     }
