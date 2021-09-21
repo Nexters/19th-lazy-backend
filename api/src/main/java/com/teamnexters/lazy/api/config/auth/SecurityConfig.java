fc82d6068dcd330d9ec3f75b0a186af4cfd6b036
YoungJun Park <youngjun108059@gmail.com>
2021-09-22 00:01:40 +0900

    [#4] OAuth - REST API 최적화 수정

--- api/src/main/java/com/teamnexters/lazy/api/config/auth/SecurityConfig.java
+++ api/src/main/java/com/teamnexters/lazy/api/config/auth/SecurityConfig.java
@@ -1,47 +1,58 @@
 package com.teamnexters.lazy.api.config.auth;
 
+import com.teamnexters.lazy.api.config.auth.jwt.JwtTokenProvider;
 import com.teamnexters.lazy.common.domain.member.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler successHandler;
+    private final JwtTokenProvider tokenProvider;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                    .authorizeRequests()
-                    .antMatchers(
-                            // 누구나 접근 가능한 설정
-                            "/",
-                            "/css/**",
-                            "/images/**",
-                            "/js/**",
-                            "/h2-console/**",
-                            "/login/**",
-                            "/swagger-ui/**",
-                            "/swagger-ui.html",
-                            "/api-docs/**"
-                    ).permitAll()
-                    .antMatchers(
-                            // "USER"인 경우만 접근 가능
-                            "/api/v1/**",
-                            "/admin/**"
-                    ).hasRole(Role.USER.name())
-                    // 나머지 URL 요청은 무조건 인증
-                    .anyRequest().authenticated()
-                .and()
-                    .logout()
-                    .logoutSuccessUrl("/") // 로그아웃 후 리다이렉팅 될 URL
-                .and()
-                    // OAuth2 로그인 관련 설정
-                    .oauth2Login()
-                    .userInfoEndpoint() // 로그인 성공 시, 사용자 정보 설정
-                    .userService(customOAuth2UserService); // 로그인 성공 후, 사용자를 다룰 서비스 등록
+            // REST API - 로그인 페이지 폼 X, csrf 보안 불필요, 토큰 기반 인증으로 세션 생성 X
+                .httpBasic().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .authorizeRequests()
+                .antMatchers(
+                        // 누구나 접근 가능한 설정
+                        "/",
+                        "/css/**",
+                        "/images/**",
+                        "/js/**",
+                        "/h2-console/**",
+                        "/login/**",
+                        "/swagger-ui/**",
+                        "/swagger-ui.html",
+                        "/api-docs/**",
+                        "/token/**"
+                ).permitAll()
+                .antMatchers(
+                        // "USER"인 경우만 접근 가능
+                        "/admin/**"
+                ).hasRole(Role.USER.name())
+                // 나머지 URL 요청은 무조건 인증
+                .anyRequest().authenticated()
+            .and()
+                // OAuth2 로그인 관련 설정
+                .oauth2Login()
+                .loginPage("/token/expired")
+                .successHandler(successHandler)
+                .userInfoEndpoint() // 로그인 성공 시, 사용자 정보 설정
+                .userService(customOAuth2UserService); // 로그인 성공 후, 사용자를 다룰 서비스 등록
+
+        http.addFilterBefore(new JwtAuthFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 }
