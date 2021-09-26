81ecd36c9338d3a522917073aaa4397f33206e98
YoungJun Park <youngjun108059@gmail.com>
2021-09-27 08:16:25 +0900

    [#4] 소셜로그인 완료 시 JWT 생성 Handler

---
+++ api/src/main/java/com/teamnexters/lazy/api/config/auth/OAuth2SuccessHandler.java
@@ -0,0 +1,48 @@
+package com.teamnexters.lazy.api.config.auth;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.teamnexters.lazy.api.config.auth.jwt.JwtTokenProvider;
+import com.teamnexters.lazy.api.config.auth.jwt.Token;
+import com.teamnexters.lazy.common.domain.member.Role;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+            throws IOException {
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        Token token = jwtTokenProvider.createToken((String) oAuth2User.getAttributes().get("email"), Role.USER);
+        log.info("{}", token);
+
+        writeTokenResponse(response, token);
+    }
+
+    private void writeTokenResponse(HttpServletResponse response, Token token)
+            throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+
+        response.addHeader("Auth", token.getAccessToken());
+        response.addHeader("Refresh", token.getRefreshToken());
+        response.setContentType("application/json;charset=UTF-8");
+
+        var writer = response.getWriter();
+        writer.println(objectMapper.writeValueAsString(token));
+        writer.flush();
+    }
+
+}
