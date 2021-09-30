e33ccee1556712a23a65d84b0752a37816a29cb6
YoungJun Park <youngjun108059@gmail.com>
2021-09-30 22:00:52 +0900

    [Fix] EC2 배포 에러 수정

--- api/src/test/java/com/teamnexters/lazy/api/LazyApiApplicationTest.java
+++ api/src/test/java/com/teamnexters/lazy/api/LazyApiApplicationTest.java
@@ -1,14 +1,8 @@
 package com.teamnexters.lazy.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class LazyApiApplicationTest {
-
-    @Test
-    void contextLoads() {
-        System.out.println("-----------------");
-    }
 
 }
