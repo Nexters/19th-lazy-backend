38da7b247c6cdd775280080d30b842eea32062cd
YoungJun Park <youngjun108059@gmail.com>
2021-10-01 23:58:29 +0900

    [#7] Error 코드 Refactoring

--- common/src/main/java/com/teamnexters/lazy/common/error/ErrorCode.java
+++ common/src/main/java/com/teamnexters/lazy/common/error/ErrorCode.java
@@ -5,23 +5,24 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
     // Common
-    INVALID_INPUT_VALUE(400, "C001", " Invalid Input Value"),
-    METHOD_NOT_ALLOWED(405, "C002", " Invalid Input Value"),
-    ENTITY_NOT_FOUND(400, "C003", " Entity Not Found"),
-    INTERNAL_SERVER_ERROR(500, "C004", "Server Error"),
-    INVALID_TYPE_VALUE(400, "C005", " Invalid Type Value"),
-    HANDLE_ACCESS_DENIED(403, "C006", "Access is Denied"),
-    TOKEN_IS_EXPIRED(407, "C007", "Token is expired"),
+    INVALID_INPUT_VALUE(400, "C001", "잘못된 요청 값입니다."),
+    INVALID_TYPE_VALUE(400, "C002", "잘못된 타입 값입니다."),
+    HANDLE_UNAUTHORIZED_JWT(401, "C003", "인증되지 않은 사용자입니다."),
+    HANDLE_ACCESS_DENIED(403, "C004", "권한이 없는 사용자입니다."),
+    ENTITY_NOT_FOUND(404, "C005", "Entity 를 찾을 수 없습니다."),
+    METHOD_NOT_ALLOWED(405, "C006", "허용되지 않은 HTTP 메소드입니다."),
+    TOKEN_IS_EXPIRED(407, "C007", "토큰이 만료되었습니다."),
 
+    INTERNAL_SERVER_ERROR(500, "C008", "서버 내부 에러입니다."),
 
     // Member
-    EMAIL_DUPLICATION(410, "M001", "Email is Duplication"),
-    NICKNAME_DUPLICATION(411, "M002", "NickName is Duplication"),
-    LOGIN_INPUT_INVALID(412, "M003", "Login input is invalid"),
-    OAUTH_PROVIDER_NOT_SUPPORT(413, "M004", "Provider is not support"),
+    EMAIL_DUPLICATION(410, "M001", "이미 등록된 이메일입니다."),
+    NICKNAME_DUPLICATION(411, "M002", "이미 등록된 닉네임입니다."),
+    LOGIN_INPUT_INVALID(412, "M003", "잘못된 로그인 요청 값입니다."),
+    OAUTH_PROVIDER_NOT_SUPPORT(413, "M004", "제공되지 않는 소셜로그인입니다."),
 
     // Habit
-    HABIT_DUPLICATION(421, "H001", "Habit is Duplication");
+    HABIT_DUPLICATION(421, "H001", "이미 등록된 습관입니다.");
 
 
     private final String code;
