873aeef70a08b029be14da62d98f96865ec461a7
YoungJun Park <youngjun108059@gmail.com>
2021-10-03 23:23:10 +0900

    [#4] OAuth2.0 로그인 시도 실패 Handler

---
+++ api/src/main/java/com/teamnexters/lazy/api/config/auth/handler/OAuth2FailureHandler.java
@@ -0,0 +1,33 @@
+package com.teamnexters.lazy.api.config.auth.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class OAuth2FailureHandler implements AuthenticationFailureHandler {
+
+    /**
+     * Called when an authentication attempt fails.
+     *
+     * @param request   the request during which the authentication attempt occurred.
+     * @param response  the response.
+     * @param exception the exception which was thrown to reject the authentication
+     */
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+            throws IOException, ServletException {
+        log.info(">>> Failure Request : {}", request);
+        log.info(">>> Failure Response : {}", response);
+        log.error(">>> Error ", exception);
+    }
+}
