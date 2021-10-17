c836384126cde06645521fa0e638efe99885ce6b
YoungJun Park <youngjun108059@gmail.com>
2021-10-17 13:27:34 +0900

    [#4] Auth Custom Annotation 작성

---
+++ common/src/main/java/com/teamnexters/lazy/common/annotation/AuthUser.java
@@ -0,0 +1,15 @@
+package com.teamnexters.lazy.common.annotation;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : member")
+public @interface AuthUser {
+
+}
