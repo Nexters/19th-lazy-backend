dba19569bbac71c171135805fcba5a220bad69a2
YoungJun Park <youngjun108059@gmail.com>
2021-10-18 23:55:07 +0900

    [#5] 습관 API - Bearer JWT 인증 추가

--- api/src/main/java/com/teamnexters/lazy/api/controller/HabitController.java
+++ api/src/main/java/com/teamnexters/lazy/api/controller/HabitController.java
@@ -3,6 +3,7 @@ package com.teamnexters.lazy.api.controller;
 import com.teamnexters.lazy.api.domain.HabitDto;
 import com.teamnexters.lazy.api.service.HabitService;
 import com.teamnexters.lazy.common.domain.habit.Habit;
+import com.teamnexters.lazy.common.domain.member.Member;
 import com.teamnexters.lazy.common.error.ErrorResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -10,11 +11,13 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,6 +32,7 @@ public class HabitController {
 
     @Operation(summary = "✅ 습관 추가 API",
             description = "습관을 추가해요.",
+            security = @SecurityRequirement(name = "bearerAuth"),
             responses = {
                     @ApiResponse(responseCode = "200", description = "[Ok] Create Habit",
                             content = @Content(schema = @Schema(implementation = HabitDto.HabitRes.class))),
@@ -36,36 +40,42 @@ public class HabitController {
                             content = @Content(schema = @Schema(implementation = ErrorResponse.class)))})
     @PostMapping("/v1/habit")
     public ResponseEntity<HabitDto.HabitRes> saveHabit(
+            @Parameter(hidden = true) Authentication authentication,
             @Parameter(description = "습관 정보", required = true) @RequestBody @Valid HabitDto.AddHabitReq dto) {
-        return ResponseEntity.ok().body(habitService.create(dto));
+        Member member = (Member) authentication.getPrincipal();
+        return ResponseEntity.ok().body(habitService.create(dto, member.getMemIdx()));
     }
 
     @Operation(summary = "✅ 전체 미루기 현황 조회 API",
-            description = "습관의 전체 미루기 현황 조회",
+            description = "습관의 전체 미루기 현황을 조회해요.",
+            security = @SecurityRequirement(name = "bearerAuth"),
             responses = {
                     @ApiResponse(responseCode = "200", description = "[Ok] Get All Habit List",
                             content = @Content(array = @ArraySchema(schema = @Schema(implementation = HabitDto.AllHabitRes.class))))})
-    @GetMapping("/v1/habit/{mem-idx}")
+    @GetMapping("/v1/habit")
     public ResponseEntity<Page<Habit>> getHabitList(
-            @Parameter(description = "회원 번호", required = true) @PathVariable(name = "mem-idx") Long memIdx,
-            @Parameter(description = "시작 페이지") @RequestParam("page") Integer page,
-            @Parameter(description = "페이지 사이즈") @RequestParam("size") Integer size) {
+            @Parameter(hidden = true) Authentication authentication,
+            @Parameter(description = "시작 페이지 (default = 0)") @RequestParam("page") Integer page,
+            @Parameter(description = "페이지 사이즈 (default = 3)") @RequestParam("size") Integer size) {
+        Member member = (Member) authentication.getPrincipal();
         if (page == null) page = 0;
         if (size == null) size = 3;
         PageRequest pageRequest = PageRequest.of(page, size);
-        return ResponseEntity.ok().body(habitService.getHabitList(memIdx, pageRequest));
+        return ResponseEntity.ok().body(habitService.getHabitList(member.getMemIdx(), pageRequest));
     }
 
     @Operation(summary = "✅ 습관 삭제 API",
             description = "습관을 삭제해요.",
+            security = @SecurityRequirement(name = "bearerAuth"),
             responses = {
                     @ApiResponse(responseCode = "200", description = "[Ok] Delete Habit",
                             content = @Content(schema = @Schema(implementation = HabitDto.HabitIndexRes.class)))})
-    @DeleteMapping("/v1/habit/{mem-idx}/{habit-idx}")
+    @DeleteMapping("/v1/habit/{habit-idx}")
     public ResponseEntity<Long> deleteHabit(
-            @Parameter(description = "회원 번호", required = true) @PathVariable(name = "mem-idx") Long memIdx,
+            @Parameter(hidden = true) Authentication authentication,
             @Parameter(description = "습관 번호", required = true) @PathVariable(name = "habit-idx") Long habitIdx) {
-        return ResponseEntity.ok().body(habitService.deleteHabit(memIdx, habitIdx));
+        Member member = (Member) authentication.getPrincipal();
+        return ResponseEntity.ok().body(habitService.deleteHabit(member.getMemIdx(), habitIdx));
     }
 
 
