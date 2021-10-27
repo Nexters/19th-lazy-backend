c5ace9c28797c4b11a619ada1194d5cc0e76b740
YoungJun Park <youngjun108059@gmail.com>
2021-10-28 00:03:12 +0900

    [#1] Feign Client 활성화

--- api/src/main/java/com/teamnexters/lazy/api/LazyApiApplication.java
+++ api/src/main/java/com/teamnexters/lazy/api/LazyApiApplication.java
@@ -3,12 +3,14 @@ package com.teamnexters.lazy.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing // JPA Auditing 활성화
+@EnableFeignClients // Feign Client 활성화
 @ComponentScan(basePackages = "com.teamnexters.lazy")
 @EnableJpaRepositories(basePackages = "com.teamnexters.lazy")
 @EntityScan(basePackages = "com.teamnexters.lazy")
